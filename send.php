f14ff9a8240b54a178ab598008b4503a4acf8ca9
Jan Henrik Lenes <jhlenes@outlook.com>
2016-12-15 20:31:58 +0100

    Clean up send.php and fix indentation in index.php

--- send.php
+++ send.php
@@ -1,22 +1,26 @@
 <?php
 
+if (isset($_GET["temp"])) {
   try {
-      $temp = $_GET["temp"];
+    $temp = $_GET["temp"];
 
-      $servername = "localhost";
-      $username = "root";
-      $password = "";
-      $database = "homebrew";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "homebrew";
 
-      $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-      $stmt = $conn->prepare("INSERT INTO `temperatur` (`temp`, `tid`) VALUES ('" . $temp . "', CURRENT_TIMESTAMP);");
-      $stmt->execute();
+    $stmt = $conn->prepare("INSERT INTO `temperatur` (`temp`, `tid`) VALUES ('" . $temp . "', CURRENT_TIMESTAMP);");
+    $stmt->execute();
 
-      echo "Succeded by sending: temp=" . $_GET["temp"];
+    echo "OK";
   } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
   }
+}else{
+  echo "Argument \"temp\" not recieved.";
+}
 
 ?>
